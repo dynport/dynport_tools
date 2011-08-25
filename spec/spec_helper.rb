66ebeaf9009f0ad8024b92344fb8a496d90218ce
Tobias Schwab <tobias.schwab@dynport.de>
2011-08-25 15:20:45 +0200

    add debugger config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,12 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'dynport_tools'
 require "timecop"
+require "ruby-debug"
+
+if defined?(Debugger) && Debugger.respond_to?(:settings)
+  Debugger.settings[:autolist] = 1
+  Debugger.settings[:autoeval] = true
+end
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
